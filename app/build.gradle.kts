59c94c2735fa95e3aee2d3f878116080b8eb2350
miker <miker@miker.org>
2022-03-07 16:57:32 -0500

    crappy fitness

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
+
+    implementation("org.apache.commons:commons-math3:3.6.1")
 }
 
 testing {
